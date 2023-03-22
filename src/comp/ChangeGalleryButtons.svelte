f60d2fdf9a7f7696d1e57895ebff046340b2c8f2
Frix63 <lederer.ch.2021@skola.ssps.cz>
2023-03-22 18:38:22 +0100

    adasaddsads

--- src/comp/ChangeGalleryButtons.svelte
+++ src/comp/ChangeGalleryButtons.svelte
@@ -5,16 +5,16 @@
 
 <div class="flex w-10/12 h-32 mx-auto flex-nowrap bg-frontground">
     <!-- svelte-ignore a11y-click-events-have-key-events -->
-    <div class="cc " on:click={() => {dispatch('changeGallery')}}>
-        <div class="flex items-center justify-center h-full text-2xl pointer-events-none ">Photos</div>
+    <div class="cc" on:click={() => {dispatch('changeGallery')}}>
+        <div class="cctext">Photos</div>
     </div>
     <!-- svelte-ignore a11y-click-events-have-key-events -->
-    <div class="cc " on:click={() => {dispatch('changeGallery1')}}>
-        <div class="flex items-center justify-center h-full text-2xl pointer-events-none ">3D</div>
+    <div class="cc" on:click={() => {dispatch('changeGallery1')}}>
+        <div class="cctext">3D</div>
     </div>
     <!-- svelte-ignore a11y-click-events-have-key-events -->
-    <div class="cc " on:click={() => {dispatch('changeGallery2')}}>
-        <div class="flex items-center justify-center h-full text-2xl pointer-events-none">Graphics</div>
+    <div class="cc" on:click={() => {dispatch('changeGallery2')}}>
+        <div class="cctext">Graphics</div>
     </div>
 </div>
 
@@ -23,5 +23,7 @@
     .cc{
         @apply w-1/3 my-3 cursor-pointer bg-secondary hover:opacity-50 text-font;
     }
-
+    .cctext {
+        @apply flex items-center justify-center h-full text-2xl pointer-events-none 
+    }
 </style>
--- src/comp/Songs.svelte
+++ src/comp/Songs.svelte
@@ -1,4 +1,6 @@
 <script>
+	import { each } from "svelte/internal";
+    import Songs from "../songs.json";
     import BreakLine from "./BreakLine.svelte";
 </script>
 
@@ -7,32 +9,9 @@
 
 
 
+{#each Songs as song}
+    <!-- svelte-ignore a11y-missing-attribute -->
+    <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="{song.link}"></iframe>
+    <BreakLine/>
+{/each}
 
-
-
-<!-- svelte-ignore a11y-missing-attribute -->
-<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1398776569&color=%23410a0d&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"></div>
-
-<BreakLine/>
-<!-- svelte-ignore a11y-missing-attribute -->
-<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1398752671&color=%230e81a4&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"></div>
-
-<BreakLine/>
-<!-- svelte-ignore a11y-missing-attribute -->
-<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1398736363&color=%231b0606&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"></div>
-
-<BreakLine/>
-<!-- svelte-ignore a11y-missing-attribute -->
-<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1359682327&color=%23444444&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"></div>
-
-<BreakLine/>
-<!-- svelte-ignore a11y-missing-attribute -->
-<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1326620509&color=%230a0808&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"></div>
-
-<BreakLine/>
-<!-- svelte-ignore a11y-missing-attribute -->
-<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1318588930&color=%23391d79&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"></div>
-
-<BreakLine/>
-<!-- svelte-ignore a11y-missing-attribute -->
-<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1006761982&color=%230c1316&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"></div>
